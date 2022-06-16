f3a89c077c7a33f59c69dbe901a6e5aa4ad4c228
NoKlapeer <noklapeer@tsn.at>
2022-06-16 17:04:12 +0200

    ljhj

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from __future__ import division
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def form():
+    return render_template('form.html')
+
+
+number = 0
+
+
+@app.route('/', methods=['POST'])
+def form_post():
+    number = request.form['number']
+    return number
+
+
+@app.route('/number')
+def calc():
+    return int(number) / 1.2
 
 
 if __name__ == '__main__':
