59025a93a37c47ff93cc029e26b047f235e859dd
NoKlapeer <noklapeer@tsn.at>
2022-06-17 12:48:27 +0200

    flaskfastfertig

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -13,7 +13,11 @@ def form():
 @app.route('/', methods=['POST'])
 def form_post():
     number = int(request.form['number'])
-    return str(number / 1.2)
+    if request.form.get('netto'):
+
+        return '''<h1 style="text-align: center; padding-top: 300px">Netto~ {} €</h1>'''.format(str(int(number / 1.2)))
+    else:
+        return '''<h1 style="text-align: center; padding-top: 300px">Brutto~ {} €</h1>'''.format(str(int(number * 1.2)))
 
 
 if __name__ == '__main__':
